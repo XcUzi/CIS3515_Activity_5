003fbc22b673bfc4769cbaae88af07a382dfd026
Jerry Lin <tuo86931@temple.edu>
2025-09-16 12:24:27 -0400

    "Fnish the assigment"

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,63 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val c: Context, private val items : Array<Int> ) : BaseAdapter() {
+
+//    val context = _context
+//    val items = _items
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+       return items.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return items[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?)=
+        if(convertView!=null){ convertView as TextView}
+        else{
+            TextView(c)
+        }.apply{
+            text = items[position].toString()
+            textSize = 22f
+        }
+//       val textView : TextView
+//
+//       if(convertView != null){
+//           textView = convertView as TextView
+//
+//       }else{
+//           textView  = TextView(context)
+//           textView.textSize = 22f
+//           textView.setPadding(5,10,0,10)
+//       }
+//
+//       textView.text =  textSize[position].toString()
+//
+//
+//
+//        return textView
+
+
+
+
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        return getView(position, convertView, parent).apply{
+            textSize = items[position].toFloat()
+
+        }
     }
+
 
 }
